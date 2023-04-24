416ec03f01945dec8aecb0a615206748e9de8144
Axel Brisse <axel.brisse@gmail.com>
2023-04-24 09:20:16 +0200

    ex02:gray_code

--- gray_code.py
+++ gray_code.py
@@ -19,3 +19,15 @@ def test_gray_code():
     assert gray_code(6) == 5
     assert gray_code(7) == 4
     assert gray_code(8) == 12
+    assert gray_code(9) == 13
+    assert gray_code(10) == 15
+    assert gray_code(11) == 14
+    assert gray_code(12) == 10
+    assert gray_code(13) == 11
+    assert gray_code(14) == 9
+    assert gray_code(15) == 8
+    assert gray_code(16) == 24
+    assert gray_code(17) == 25
+    assert gray_code(18) == 27
+    assert gray_code(19) == 26
+    assert gray_code(20) == 30
