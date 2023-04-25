fe1d43130bc0ca5fd5afa175e08b29a5524c11fb
norxondor_gorgonax <axel.brisse@gmail.com>
2023-04-25 06:49:04 +0200

    test formula checks and fix is_valid_nnf

---
+++ tests/test_formula_checks.py
@@ -0,0 +1,61 @@
+from srcs.formula_checks import (
+    is_valid_explicit_formula,
+    is_valid_formula,
+    is_valid_nnf,
+    is_valid_variable_formula,
+)
+
+
+def test_is_valid_formula():
+    assert is_valid_formula("0")
+    assert is_valid_formula("A")
+    assert is_valid_formula("A!B!|")
+    assert is_valid_formula("0!1!&")
+    assert is_valid_formula("A!B|")
+    assert is_valid_formula("AB&1!B!&|")
+    assert is_valid_formula("A!B!&0!|")
+    assert is_valid_formula("AB&!")
+    assert is_valid_formula("AB|!")
+    assert is_valid_formula("AB>")
+    assert is_valid_formula("AB=")
+    assert is_valid_formula("AB|C&!")
+
+
+def test_is_invalid_formula():
+    assert not is_valid_formula(1)
+    assert not is_valid_formula("")
+    assert not is_valid_formula("!")
+    assert not is_valid_formula("&")
+    assert not is_valid_formula("A&B")
+    assert not is_valid_formula("AB!!")
+    assert not is_valid_formula("AB^^")
+    assert not is_valid_formula("ABC|")
+    assert not is_valid_formula("ABC|D&")
+
+
+def test_is_valid_explicit_formula():
+    assert is_valid_explicit_formula("0!1!&")
+    assert not is_valid_explicit_formula("A!B!&0!|")
+    assert not is_valid_explicit_formula("AB&!")
+
+
+def test_is_valid_variable_formula():
+    assert not is_valid_variable_formula("0!1!&")
+    assert not is_valid_variable_formula("A!B!&0!|")
+    assert is_valid_variable_formula("AB&!")
+
+
+def test_is_valid_nnf():
+    assert is_valid_nnf("A!B!|")
+    assert is_valid_nnf("A!B!&")
+    assert is_valid_nnf("A!B|")
+    assert is_valid_nnf("AB&A!B!&|")
+    assert is_valid_nnf("A!B!&C!|")
+
+
+def test_is_invalid_nnf():
+    assert not is_valid_nnf("AB&!")
+    assert not is_valid_nnf("AB|!")
+    assert not is_valid_nnf("AB>")
+    assert not is_valid_nnf("AB=")
+    assert not is_valid_nnf("AB|C&!")
