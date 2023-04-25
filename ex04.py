0901064f069ed25087fad57d7665a65f4c8fbd11
norxondor_gorgonax <axel.brisse@gmail.com>
2023-04-25 02:24:13 +0200

    ex04

---
+++ ex04.py
@@ -0,0 +1,21 @@
+from ex03 import eval_formula
+from itertools import product
+from string import ascii_uppercase
+
+ALLOWED_CHARACTERS = set(ascii_uppercase + "!&|^>=")
+
+
+def get_truth_table(s):
+    letters = "".join(sorted(set(filter(str.isupper, s))))
+    table = [[*letters, "="], ["---"] * (len(letters) + 1)]
+    for values in product("01", repeat=len(letters)):
+        formula = s.translate(str.maketrans(letters, "".join(values)))
+        result = eval_formula(formula)
+        table.append([*values, "1" if result else "0"])
+    print(table)
+    return table
+
+
+def print_truth_table(s):
+    for row in get_truth_table(s):
+        print("|" + "|".join(x.center(3) for x in row) + "|")
