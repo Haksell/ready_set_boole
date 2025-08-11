8692b1ae333c8259c313e77b5fb4a1cae2f5c1ec
Haksell <axel.brisse@gmail.com>
2025-08-11 21:08:48 +0200

    fix ex01 args

--- src/bin/ex01.rs
+++ src/bin/ex01.rs
@@ -1,15 +1,20 @@
-use ready_set_boole::multiplier;
+use {itertools::Itertools, ready_set_boole::multiplier};
 
 fn main() {
+    let args = std::env::args().skip(1).collect_vec();
     assert!(
-        std::env::args().len() >= 2,
+        args.len() >= 1,
         "Usage: cargo run -q --bin ex01 n1 n2 n3 ..."
     );
-    let mut res = 1;
-    for (i, arg) in std::env::args().skip(1).enumerate() {
-        let n = arg.parse().expect("failed to parse u32");
-        res = multiplier(res, n);
-        print!("{}{} ", if i == 0 { "" } else { "* " }, n);
-    }
-    println!("= {res}");
+    let nums: Vec<u32> = args
+        .iter()
+        .map(|arg| {
+            arg.parse()
+                .expect(&format!("failed to parse \"{}\" as u32", arg))
+        })
+        .collect();
+
+    let lhs = nums.iter().map(u32::to_string).join(" + ");
+    let rhs = nums.iter().fold(1, |sum, &n| multiplier(sum, n));
+    println!("{lhs} = {rhs}");
 }
