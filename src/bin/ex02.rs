81da733ccab394b44e273c4a2c0c65d103bb7c99
Haksell <axel.brisse@gmail.com>
2025-08-12 16:26:40 +0200

    ex02: 42

--- src/bin/ex02.rs
+++ src/bin/ex02.rs
@@ -1,8 +1,8 @@
 use ready_set_boole::gray_code;
 
 fn main() {
-    for i in 0..21 {
+    for i in 0..=42 {
         let gc = gray_code(i);
-        println!("{:2} | {:2} | {:05b}", i, gc, gc);
+        println!("{:2} | {:2} | {:06b}", i, gc, gc);
     }
 }
